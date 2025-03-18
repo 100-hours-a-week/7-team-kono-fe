a7e6db5d1eb755e5a60bdd4f35a6a8ff02d13788
hayden <haydenhyo@gmail.com>
2025-03-18 15:58:00 +0900

    chore: remove AuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { AuthProvider } from './contexts/AuthContext';
+import { ThemeProvider } from './contexts/ThemeContext';
+import App from './App';
 import './index.css';
-import App from './App.tsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import NotFound from './pages/NotFound.tsx';
 import Wallet from './pages/Wallet.tsx';
@@ -15,7 +17,7 @@ import Trade from './pages/Trade.tsx';
 import Transaction from './pages/Transaction.tsx';
 import Login from './pages/Login.tsx';
 import KakaoCallback from './pages/KakaoCallback.tsx';
-import { ThemeProvider } from './contexts/ThemeContext.tsx';
+import Layout from './components/layout/Layout';
 
 const router = createBrowserRouter([
   {
@@ -24,81 +26,74 @@ const router = createBrowserRouter([
     errorElement: <NotFound />,
     children: [
       {
-        index: true,
-        element: <Favorite />,
-      },
-      {
-        path: 'login',
-        element: <Login />,
-      },
-      {
-        path: 'wallet',
-        element: <Wallet />,
-      },
-      {
-        path: 'discover',
-        element: <Discover />,
-      },
-      {
-        path: 'favorites',
-        element: <Favorite />,
-      },
-      {
-        path: 'rankings',
-        element: <Ranking />,
-      },
-      {
-        path: 'settings',
-        element: <Settings />,
-      },
-      {
-        path: 'profile',
-        element: <Profile />,
-      },
-      {
-        path: 'users/:nickname',
-        element: <Profile />,
-      },
-      // {
-      //   path: 'trade',
-      //   element: <Trade />,
-      // },
-      // {
-      //   path: 'trade/:ticker',
-      //   element: <Trade />,
-      // },
-      {
-        path: 'transaction',
-        element: <Transaction />,
-      },
-      // {
-      //   path: 'coins/:ticker/buy',
-      //   element: <Trade />,
-      // },
-      // {
-      //   path: 'coins/:ticker/sell',
-      //   element: <Trade />,
-      // },
-      {
-        path: 'coins/:ticker/:type',
-        element: <Trade />,
-      },
-      {
-        path: 'coins/:ticker',
-        element: <CoinDetail />,
-      },
-      {
-        path: 'oauth/kakao/callback',
-        element: <KakaoCallback />,
+        element: <Layout />,
+        children: [
+          {
+            index: true,
+            element: <Favorite />,
+          },
+          {
+            path: 'login',
+            element: <Login />,
+          },
+          {
+            path: 'wallet',
+            element: <Wallet />,
+          },
+          {
+            path: 'discover',
+            element: <Discover />,
+          },
+          {
+            path: 'favorites',
+            element: <Favorite />,
+          },
+          {
+            path: 'rankings',
+            element: <Ranking />,
+          },
+          {
+            path: 'settings',
+            element: <Settings />,
+          },
+          {
+            path: 'profile',
+            element: <Profile />,
+          },
+          {
+            path: 'users/:nickname',
+            element: <Profile />,
+          },
+          {
+            path: 'transaction',
+            element: <Transaction />,
+          },
+          {
+            path: 'coins/:ticker/:type',
+            element: <Trade />,
+          },
+          {
+            path: 'coins/:ticker',
+            element: <CoinDetail />,
+          },
+          {
+            path: 'coins/ticker',
+            element: <CoinDetail />,
+          },
+          {
+            path: 'oauth/kakao/callback',
+            element: <KakaoCallback />,
+          },
+        ],
       },
     ],
   },
 ]);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <ThemeProvider>
       <RouterProvider router={router} />
     </ThemeProvider>
-  </StrictMode>,
+  </React.StrictMode>,
 );
